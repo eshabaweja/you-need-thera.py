9563a3cbea6ff113ddd7241ad672600eaad0161f
eshabaweja <bawejaesha02@gmail.com>
2022-06-11 13:09:42 +0530

    day 1 done

---
+++ day-1/band-name-generator.py
@@ -0,0 +1,8 @@
+# band name generator 
+# string concatenation
+print("WELCOME TO THE BAND NAME GENERATOR.")
+print("Let me help you find your band name!\n")
+bev = input("What's your favorite beverage?\n")
+adj = input("One word to describe your pet:\n")
+
+print("Your band name is ", adj, bev,"\n")
