7b400f67c58449c43203273b0589a18fcb16a8a6
eshabaweja <bawejaesha02@gmail.com>
2022-12-20 17:01:51 +0530

    looks good. next: fetch and render from api

--- day_59/upgraded-blog/main.py
+++ day_59/upgraded-blog/main.py
@@ -4,9 +4,19 @@ app = Flask(__name__)
 
 # Home page rendered
 @app.route("/")
+@app.route("/index.html")
 def home():
     return render_template('index.html')
 
+# About page rendered
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+# Contact page rendered
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
 
 
 
