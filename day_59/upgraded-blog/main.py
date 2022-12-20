347a768e39ded285cb243260eea72733e1e7974d
eshabaweja <bawejaesha02@gmail.com>
2022-12-20 22:04:32 +0530

    day 59: blog capstone styling

--- day_59/upgraded-blog/main.py
+++ day_59/upgraded-blog/main.py
@@ -20,8 +20,11 @@ def about():
 def contact():
     return render_template("contact.html")
 
-
-
+# Posts rendered dynamically
+""" You can add variable sections to a URL by marking sections with <variable_name>. Your function then receives the <variable_name> as a keyword argument. Optionally, you can use a converter to specify the type of the argument like <converter:variable_name>. """
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    return render_template("post.html", posts = api_fetch, post_index = post_id-1)
 
 
 ########### notes ###########
