d6535daaf0639fdc06c7a9668efa9a39a20b8702
eshabaweja <bawejaesha02@gmail.com>
2022-12-27 22:03:03 +0530

    before SQLite

---
+++ day_63/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template('index.html',all_books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if(request.method == "POST"):
+        all_books.append({
+        "title": request.form["bookname"],
+        "author": request.form["bookauthor"],
+        "rating": request.form["bookrating"]})
+        # print(all_books)
+    return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
